4a08245ae4c40880be6b1a71f982082435da9545
yogesh kumar <89630781+yogeskumar@users.noreply.github.com>
2021-12-21 22:25:31 +0530

    adding login and register pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Accesories from './components/accesories/Accesories';
 import Cart from './components/Cart';
 import Footer from './components/footer/Footer';
 import Home from './components/home/Home';
+import Login from './components/Login';
 import NavBar from './components/navbar/NavBar';
+import Register from './components/Register';
 
 function App () {
   return (
@@ -15,6 +17,8 @@ function App () {
         <Route path="/" element={<Home />} />
         <Route path="/accesories" element={<Accesories />} />
         <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
